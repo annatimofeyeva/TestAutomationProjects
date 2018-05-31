88510497cbb2c047e712f00c9fb24c3ba8ce585c
annaTi <anjatimofeyeva@gmail.com>
2018-05-31 15:57:02 -0700

    test2

--- src/main/java/FoorLoop.java
+++ src/main/java/FoorLoop.java
@@ -4,5 +4,6 @@ public class FoorLoop {
     public static void main(String[] args) {
         System.out.println("Test");
         System.out.println("Test1");
+        System.out.println("Test2");
     }
 }
