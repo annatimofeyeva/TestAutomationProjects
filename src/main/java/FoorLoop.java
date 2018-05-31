f634ce120339ac8bb1f428f81326ea8e4c79c05b
annaTi <anjatimofeyeva@gmail.com>
2018-05-31 15:56:00 -0700

    test1

--- src/main/java/FoorLoop.java
+++ src/main/java/FoorLoop.java
@@ -3,5 +3,6 @@ import org.testng.annotations.Test;
 public class FoorLoop {
     public static void main(String[] args) {
         System.out.println("Test");
+        System.out.println("Test1");
     }
 }
