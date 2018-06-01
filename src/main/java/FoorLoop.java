fb8d209869384fad3d7d06fc7159358bf9981ab8
annaTi <anjatimofeyeva@gmail.com>
2018-05-31 20:15:22 -0700

    odd and even numbers code added

--- src/main/java/FoorLoop.java
+++ src/main/java/FoorLoop.java
@@ -3,17 +3,34 @@ public class FoorLoop {
         // sout - System.out.println();
 
         // Printing odd numbers:
+
         for (int i = 0; i <= 10; i++) {
-            if (i % 2 == 0) {
-                System.out.println(i + " = "+ "even");
-            }else
-                System.out.println(i + " = " + "odd");
+            int x = i%2;   // x = reminder
+            if(x == 1) {
+                System.out.println("Odd number: " + i);
+            }
+//
+//            if(x == 0) {
+//                System.out.println("Even number: " + i);
+//            }
         }
 
-        // Printing even number:
-        System.out.println("Even numbers: ");
-        for (int i = 0; i <= 10; i = i + 2) {
-            System.out.println(i);
-        }
+//         Printing odd numbers #2:
+
+//            if (i%2 == 0) {
+//                System.out.println(i + " = "+ "even");
+//            }else
+//                System.out.println(i + " = " + "odd");
+//        }
+//
+//            Printing even number #2:
+//        System.out.println("Even numbers: ");
+//        for (int i = 0; i <= 10; i = i + 2) {
+//            System.out.println(i);
+//        }
+
+        // Printing Prime numbers. Prime numbers - which are dividible only by 1 and itself
+
+
     }
 }
