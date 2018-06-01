9d6cdb574a16013fd587bd85af8aa63c6d84771c
annaTi <anjatimofeyeva@gmail.com>
2018-05-31 21:34:39 -0700

    Fibonachi numbers code added

--- src/main/java/FoorLoop.java
+++ src/main/java/FoorLoop.java
@@ -50,5 +50,18 @@ public class FoorLoop {
                 System.out.println("Prime number: " + i);
             }
         }
-    }
-}
+        System.out.println("Fibonachi numbers: ");
+
+        int n1 = 0;
+        int n2 = 1;
+        int n = n1+n2;
+
+        while (n<1000){
+            System.out.printf("%d ", n);
+            n1 = n2;
+            n2 = n;
+            n = n1 + n2;
+        }
+
+    }// end of main
+} // end of class
