df7574e94ffe8f9195af3b48537854456cc8f63f
annatimofeyeva <anjatimofeyeva@gmail.com>
2018-05-31 21:54:09 -0700

    some minor fixes

--- src/main/java/FoorLoop.java
+++ src/main/java/FoorLoop.java
@@ -1,7 +1,7 @@
 public class FoorLoop {
     public static void main(String[] args) {
-        // sout - System.out.println();
 
+        // sout - System.out.println();
         // Printing odd numbers:
 
         for (int i = 0; i <= 10; i++) {
@@ -14,7 +14,6 @@ public class FoorLoop {
 //                System.out.println("Even number: " + i);
 //            }
         }
-
 //         Printing odd numbers #2:
 
 //            if (i%2 == 0) {
@@ -22,8 +21,7 @@ public class FoorLoop {
 //            }else
 //                System.out.println(i + " = " + "odd");
 //        }
-//
-//            Printing even number #2:
+////            Printing even number #2:
 //        System.out.println("Even numbers: ");
 //        for (int i = 0; i <= 10; i = i + 2) {
 //            System.out.println(i);
@@ -35,33 +33,32 @@ public class FoorLoop {
         // 1. Take some number
         // 2. Divide number from 2 to (self -1)
         // 3. If not divisible - then prime
-        // 4.
 
         for (int i = 2; i <= 50; i++) {
             boolean isPrime = true;
-            for (int j = 2; j < i/2; j++) {
+            for (int j = 2; j < i / 2; j++) {
                 int x = i % j;
                 if (x == 0) {
                     isPrime = false;
                     break;
                 }
             }
-            if (isPrime) {
+                if (isPrime) {
                 System.out.println("Prime number: " + i);
             }
         }
-        System.out.println("Fibonachi numbers: ");
+
+        System.out.println("Printed Fibonachi numbers: ");
 
         int n1 = 0;
         int n2 = 1;
-        int n = n1+n2;
+        int n = n1 + n2;
 
-        while (n<1000){
+        while (n < 1000) {
             System.out.printf("%d ", n);
             n1 = n2;
             n2 = n;
             n = n1 + n2;
         }
-
     }// end of main
 } // end of class
