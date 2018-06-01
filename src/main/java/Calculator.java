5658f909c4682b84a3c8df6a47ce53ab13ee6fe8
annatimofeyeva <anjatimofeyeva@gmail.com>
2018-06-01 16:25:45 -0700

    Overloading exercise

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,36 @@
+public class Calculator {
+
+    // exercise for Polymorfism and Overloading
+    // Constructor
+
+    public Calculator() {
+    }
+
+    public static void main(String[] args) {
+        Calculator cal = new Calculator();
+        double x = cal.addNumbers(2, 5);
+        System.out.println(x);
+        double y = cal.addNumbers(3, 20, 5);
+        System.out.println(y);
+        double z = cal.addNumbers(3, 20, 5, 55);
+        System.out.println(z);
+    }
+
+    public double addNumbers(double a, double b) {
+        double sum;
+        sum = a + b;
+        return sum;
+    }
+
+    public double addNumbers(double a, double b, double c) {
+        double sum;
+        sum = a + b + c;
+        return sum;
+    }
+
+    public double addNumbers(double a, double b, double c, double d) {
+        double sum;
+        sum = a + b + c + d;
+        return sum;
+    }
+}
---
+++ src/main/java/CitBankAccount.java
@@ -0,0 +1,31 @@
+public class CitBankAccount extends BankAccountSimple {
+    // Constructor
+    public CitBankAccount(int accountNumber, double balance, String customerName) {
+        super(accountNumber, balance, customerName);
+    }
+
+    public static void main(String[] args) {
+        CitBankAccount account1 = new CitBankAccount(1111, 2000, "Anna");
+        CitBankAccount account2 = new CitBankAccount(1111, 3000, "Alla");
+        CitBankAccount account3 = new CitBankAccount(1111, 4000, "Lynn");
+        CitBankAccount account4 = new CitBankAccount(1111, 5000, "Emily");
+        CitBankAccount account5 = new CitBankAccount(1111, 6000, "Alena");
+
+        CitBankAccount[] accounts= new CitBankAccount[5];
+
+        accounts[0] = account1;
+        accounts[1] = account2;
+        accounts[2] = account3;
+        accounts[3] = account4;
+        accounts[4] = account5;
+
+        String customer3name = accounts[3].getCustomerName();
+        System.out.println(customer3name);
+
+        for (int i = 0; i < accounts.length; i++) {
+//            accounts[i].getBalance();
+            double x = accounts[i].getBalance();
+            System.out.println(x);
+        }
+    }
+}
