b87584cd5498b6a0b6f925a62642be44f3cab626
annatimofeyeva <anjatimofeyeva@gmail.com>
2018-05-31 23:20:43 -0700

    fixed minor issues

--- src/main/java/FoorLoop.java
+++ src/main/java/FoorLoop.java
@@ -1,6 +1,5 @@
 public class FoorLoop {
     public static void main(String[] args) {
-
         // sout - System.out.println();
         // Printing odd numbers:
 
@@ -9,31 +8,31 @@ public class FoorLoop {
             if (x == 1) {
                 System.out.println("Odd number: " + i);
             }
-//
 //            if(x == 0) {
 //                System.out.println("Even number: " + i);
 //            }
         }
-//         Printing odd numbers #2:
+        /*
+         Printing odd numbers #2:
 
-//            if (i%2 == 0) {
-//                System.out.println(i + " = "+ "even");
-//            }else
-//                System.out.println(i + " = " + "odd");
-//        }
-////            Printing even number #2:
-//        System.out.println("Even numbers: ");
-//        for (int i = 0; i <= 10; i = i + 2) {
-//            System.out.println(i);
-//        }
+            if (i%2 == 0) {
+                System.out.println(i + " = "+ "even");
+            }else
+                System.out.println(i + " = " + "odd");
+        }
+            Printing even number #2:
+        System.out.println("Even numbers: ");
+        for (int i = 0; i <= 10; i = i + 2) {
+            System.out.println(i);
+        }
+        */
+        /* Printing Prime numbers. Prime numbers - which are dividible only by 1 and itself
+         Prime numbers always stars from 2
 
-        // Printing Prime numbers. Prime numbers - which are dividible only by 1 and itself
-        // Prime numbers always stars from 2
-
-        // 1. Take some number
-        // 2. Divide number from 2 to (self -1)
-        // 3. If not divisible - then prime
-
+         1. Take some number
+         2. Divide number from 2 to (self -1)
+         3. If not divisible - then prime
+        */
         for (int i = 2; i <= 50; i++) {
             boolean isPrime = true;
             for (int j = 2; j < i / 2; j++) {
