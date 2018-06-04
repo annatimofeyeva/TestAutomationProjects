a93ca037aeb8a68480b492f79faab9bcea1ded65
annatimofeyeva <anjatimofeyeva@gmail.com>
2018-06-04 11:43:38 -0700

    JavaArrayExercises added

---
+++ src/main/java/JavaArrayExercises.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class JavaArrayExercises {
+    public static void main(String[] args) {
+
+        //Write a Java program to sum values of an array.
+
+        int[] myArray = {1, 2, 3, 4, 5};
+        int sum = 0;
+        for (int i : myArray)
+            sum += i;
+        System.out.println("Sum of all integers is: " + sum);
+
+        Double[] myDouble = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0};
+        double myDoubleSum = 0;
+        for (double i : myDouble) {
+            myDoubleSum += i;
+            System.out.println("Sum of all doubles is: " + myDoubleSum);
+        }
+    }
+}
