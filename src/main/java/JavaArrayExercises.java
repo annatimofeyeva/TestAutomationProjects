b6f113c3053de82c547da2a4360a1bbf4316a20c
annatimofeyeva <anjatimofeyeva@gmail.com>
2018-06-04 12:00:18 -0700

    minor code changes added

--- src/main/java/JavaArrayExercises.java
+++ src/main/java/JavaArrayExercises.java
@@ -9,15 +9,27 @@ public class JavaArrayExercises {
         int sum = 0;
         for (int i : myArray)
             sum += i;
-        System.out.println("Sum of all integers is: " + sum);
+        System.out.println("Sum of integers in the first array is: " + sum);
+
+        int[] secondInt = {2, 4, 6, 8, 10};
+        int sumOne = 0;
+        for (int i : secondInt)
+            sumOne = sumOne + i;
+        System.out.println("Sum of integers in the second array is: " + sumOne);
 
         Double[] myDouble = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0};
         double myDoubleSum = 0;
         for (double i : myDouble) {
             myDoubleSum += i;
-            if(i > 5) {
-                System.out.println("Sum of all doubles is: " + myDoubleSum);
+            if (i > 5) {
+                System.out.println("Sum of doubles in the first array is: " + myDoubleSum);
             }
         }
+
+        Double[] second = {2.0, 4.0, 6.0, 8.0};
+        double result = 0.0;
+        for (double i : second)
+            result += i;
+        System.out.println("Sum of doubles in the second array is: " + result);
     } // end of main
 } // end of class
