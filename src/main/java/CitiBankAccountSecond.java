dbcbd20217dbd9942bdb8f97db6435404f3db8e6
annatimofeyeva <anjatimofeyeva@gmail.com>
2018-06-04 15:38:59 -0700

    minor issues in interface implementation fixed

--- src/main/java/CitiBankAccountSecond.java
+++ src/main/java/CitiBankAccountSecond.java
@@ -12,24 +12,39 @@ public class CitiBankAccountSecond implements BankAccount {
 
     public static void main(String[] args) {
 
-        CitiBankAccountSecond citi1 = new CitiBankAccountSecond(111, 2000, "Alla");
-        CitiBankAccountSecond citi2 = new CitiBankAccountSecond(222, 2000, "Anna");
-        CitiBankAccountSecond citi3 = new CitiBankAccountSecond(333, 2000, "Steve");
-        CitiBankAccountSecond citi4 = new CitiBankAccountSecond(444, 2000, "Natasha");
-        CitiBankAccountSecond citi5 = new CitiBankAccountSecond(555, 2000, "Nicole");
+        CitiBankAccountSecond citi1 =
+                new CitiBankAccountSecond(111, 1, "Alla");
+        CitiBankAccountSecond citi2 =
+                new CitiBankAccountSecond(222, 1, "Anna");
+        CitiBankAccountSecond citi3 =
+                new CitiBankAccountSecond(333, 1, "Steve");
+        CitiBankAccountSecond citi4 =
+                new CitiBankAccountSecond(444, 1, "Mila");
+        CitiBankAccountSecond citi5 =
+                new CitiBankAccountSecond(555, 1, "Nicole");
 
-        HSBSBankAccountSecond hsbs1 = new HSBSBankAccountSecond(999, 3000, "Kirill");
+        HSBSBankAccountSecond hsbs1 =
+                new HSBSBankAccountSecond(666, 1, "Daniel");
+        HSBSBankAccountSecond hsbs2 =
+                new HSBSBankAccountSecond(777, 1, "Gabriel");
+        HSBSBankAccountSecond hsbs3 =
+                new HSBSBankAccountSecond(888, 1, "Harry");
+        HSBSBankAccountSecond hsbs4 =
+                new HSBSBankAccountSecond(999, 1, "Albert");
+        HSBSBankAccountSecond hsbs5 =
+                new HSBSBankAccountSecond(1000, 1, "Bryan");
 
-        BankAccount[] allCitiAccounts = {citi1, citi2, citi3, citi3, citi4, citi5, hsbs1};
-        String name = allCitiAccounts[6].getCustomerName();
-        System.out.println("Customer name of HSBS class (index3) is: " + name);
+        BankAccount[] allCitiAccounts =
+                {citi1, citi2, citi3, citi4, citi5, hsbs1, hsbs2, hsbs3, hsbs4, hsbs5};
+        String name = allCitiAccounts[2].getCustomerName();
+        System.out.println("Customer name of HSBS class (index2) is: " + name);
 
         double totalSum = 0.0;
 
         for (int i = 0; i < allCitiAccounts.length; i++) {
             double balanceOfEachPerson = allCitiAccounts[i].getBalance();
             totalSum = totalSum + balanceOfEachPerson;
-            if (i >= 6) {
+            if (i >=9) {
                 System.out.println("Total balance is: " + totalSum);
             }
         }
--- src/main/java/HSBSBankAccountSecond.java
+++ src/main/java/HSBSBankAccountSecond.java
@@ -9,7 +9,9 @@ public class HSBSBankAccountSecond implements BankAccount {
         this.customerName = customerName;
     }
 
-    public static void main(String[] args) {HSBSBankAccountSecond hsbs1 = new HSBSBankAccountSecond(111, 5000, "John");
+    public static void main(String[] args) {
+        HSBSBankAccountSecond hsbs1 =
+                new HSBSBankAccountSecond(111, 5000, "John");
         HSBSBankAccountSecond hsbs2 =
                 new HSBSBankAccountSecond(222, 6000, "Mary");
         HSBSBankAccountSecond hsbs3 =
@@ -23,6 +25,7 @@ public class HSBSBankAccountSecond implements BankAccount {
         BankAccount[] allHsbsAccounts = {hsbs1, hsbs2, hsbs3, hsbs4, hsbs5};
         String name = allHsbsAccounts[3].getCustomerName();
         System.out.println("Customer name  of third index: " + name);
+        System.out.println("Customer 4 account number: " + allHsbsAccounts[3].getAccountNumber());
     }
 
     public int getAccountNumber() {
