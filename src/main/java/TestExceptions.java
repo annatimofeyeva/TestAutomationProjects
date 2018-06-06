f2229de177191b34bc3c5a8d9cb465fda1d882c2
annatimofeyeva <anjatimofeyeva@gmail.com>
2018-06-05 23:23:24 -0700

    minor fixes in TestExceptions class

--- src/main/java/TestExceptions.java
+++ src/main/java/TestExceptions.java
@@ -1,10 +1,11 @@
 public class TestExceptions {
     public static void main(String[] args) {
-        BankAccountSimple b = new BankAccountSimple(111, 300, "John");
-        try{
-            b.withDraw(500);
-        }catch(Exception e){
+        BankAccountSimple b = new BankAccountSimple(111, 1000, "John");
+        try {
+            b.withDraw(200);
+        } catch (Exception e) {
             System.out.println("You don't have sufficient balance");
         }
+        System.out.println("You have enougth amount for withdraw");
     }
 }
