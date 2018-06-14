3578d61bd0afa9e6583e86db9b2e3af4e17279a8
annatimofeyeva <anjatimofeyeva@gmail.com>
2018-06-13 19:26:47 -0700

    @BeforeTest, @Test, @AfterTest added

--- src/main/FitsAutomationTests/TestNGChromeFitsAutomation.java
+++ src/main/FitsAutomationTests/TestNGChromeFitsAutomation.java
@@ -2,6 +2,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -9,13 +10,11 @@ import java.util.concurrent.TimeUnit;
 
 public class TestNGChromeFitsAutomation {
 
-    private ChromeDriver driver;
+    WebDriver driver;
 
     @BeforeTest
     public void openBrowser() {
 
-
-        WebDriver driver;
         String baseUrl;
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
@@ -23,13 +22,10 @@ public class TestNGChromeFitsAutomation {
         driver.manage().window().maximize();
         baseUrl = ("http://fits.qauber.com");
         driver.get(baseUrl);
-        System.out.println("Hello");
     }
 
-    @Test
-    public void login() throws InterruptedException {
-        System.out.println("Hi");
-
+    @Test (groups= {"functional", "UI"} )
+    public void testLogin() throws InterruptedException {
         WebElement emailTextBox = driver.findElement(By.xpath("//input[@id='exampleInputEmail1']"));
         emailTextBox.sendKeys("annatimofeyeva@yandex.ru");
         WebElement passwordTextBox = driver.findElement(By.xpath("//input[@id='exampleInputPassword1']"));
@@ -37,6 +33,22 @@ public class TestNGChromeFitsAutomation {
         WebElement loginButton = driver.findElement(By.xpath("//button[text() = 'Login']"));
         Thread.sleep(2000);
         loginButton.click();
+    }
+
+    @Test(dependsOnMethods = {"testLogin"}, groups={"functional", "UI"}, description = "This is a test for adding new report", enabled = false)
+    public void addReport() {
+
 
     }
-}
+
+    @Test(dependsOnMethods = {"addReport","testLogin"}, groups={"functional", "UI"}, enabled = false)
+    public void searchReport() {
+
+    }
+
+
+   @AfterTest
+    public void closeBroser() {
+        driver.quit();
+   }
+} // end of class
