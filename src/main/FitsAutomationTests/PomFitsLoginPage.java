2cda1f636bb8cc29fcf32190dda44ce7bcb8b8dd
annatimofeyeva <anjatimofeyeva@gmail.com>
2018-07-02 19:29:41 -0700

    POM class for FitsLoginPage created

---
+++ src/main/FitsAutomationTests/PomFitsLoginPage.java
@@ -0,0 +1,36 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+// created a Class for Login page, so all the Xpaths are stored in one place
+
+public class PomFitsLoginPage {
+    WebDriver wb;
+
+    @FindBy(xpath="//input[@id='exampleInputEmail1']")
+    WebElement usernameTextBox;
+    @FindBy(xpath="//input[@id='exampleInputPassword1']")
+    WebElement passwordTextBox;
+    @FindBy(xpath="//button[text() = 'Login']")
+    WebElement loginButton;
+
+    public PomFitsLoginPage(WebDriver wb){
+        this.wb = wb;
+        PageFactory.initElements(wb, this);
+    }
+
+    public void typeUserName(String userName){
+        usernameTextBox.sendKeys(userName);
+    }
+
+    public void typePassword(String password){
+        passwordTextBox.sendKeys(password);
+    }
+
+    public void clickLoginButton(){
+        loginButton.click();
+    }
+
+
+}
